6eff66c3d1c70d283e878a6ca755bc8648c8132e
Arun NS <arun.natarajan52@gmail.com>
2022-12-12 14:35:35 +0000

    RecyclerView Adapter Added

--- app/src/main/java/com/omaemirates/Adapter.kt
+++ app/src/main/java/com/omaemirates/Adapter.kt
@@ -1,4 +1,24 @@
 package com.omaemirates
 
-class Adapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class Adapter : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+    }
 }
